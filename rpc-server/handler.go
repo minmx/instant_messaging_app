40cfa1ee876d0ea97fb59ad8eb85fddc6d4a1221
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 23:55:09 +0800

    added Send() and Pull()

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"math/rand"
 	"strings"
+	"time"
 	// "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 	"../kitex_gen/rpc"
 )
@@ -63,4 +64,65 @@ func ValidateRequest(req *rpc.SendRequest){
 		return err
 	}
 	return nil
+}
+
+func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error){
+	if err := ValidateRequest(req); err != nil {
+		return nil, err
+	}
+	timestamp := time.Now().Unix()
+	message := &Message{
+		Message: req.Message.GetText(),
+		Sender: req.Message.GetSender(),
+		Timestamp: timestamp
+	}
+	roomID := GetRoomID(req.Message.GetChat())
+	err := rdb.SaveMessage(ctx, roomID, message)
+	if err != nil {
+		return nil, err
+	}
+	resp := rpc.NewSendResponse()
+	resp.Code, resp.Msg = 0, "success"
+    return resp, nil
+}
+
+func (s * IMServiceImpl) Pull(ctx context.Context, req rpc.PullRequest) (*rpc.PullResponse, error){
+	roomID, err := GetRoomID(req.GetChat())
+	if err != nil {
+		return nil, err
+	}
+
+	start := req.GetCursor()
+	end := start + int64(req.GetLimit())
+
+	messages, err := rdb.GetMessage(ctx, roomID, start, end, req.GetReverse())
+	if err != nil {
+		return nil, err
+	}
+
+	respMessage := make([]*rpc.Message, 0)
+	var counter int32 = 0
+	var nextCursor int64 = 0
+	hasMore := false
+	for _, msg := range messages {
+		if counter+1 > req.GetLimit() {
+			hasMore = true
+			nextCursor = end
+			break
+		}
+		temp := &rpc.Message{
+			Chat: req.GetChat(),
+            Text: msg.Message,
+            Sender: msg.Sender,
+            SendTime: msg.Timestamp
+       }
+	   resp := rpc.NewPullResponse()
+	   resp.Messages = respMessage
+	   resp.Code = 0
+	   resp.Msg = "success"
+	   resp.HasMore = &hasMore
+	   resp.NextCursor = &nextCursor
+
+	   return resp, nil
+	}
 }
