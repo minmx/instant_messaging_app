3121b42c8d3303c985834a46df375da6e41906cb
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 22:20:49 +0800

    added ValidateRequest to handler.go

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"math/rand"
 	"strings"
-	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
+	// "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
+	"../kitex_gen/rpc"
 )
 
 // IMServiceImpl implements the last service interface defined in the IDL.
@@ -26,7 +27,7 @@ func areYouLucky() (int32, string) {
 	if rand.Int31n(2) == 1 {
 		return 0, "success"
 	} else {
-		return 500, "oops"
+		return 500, "unlucky"
 	}
 }
 
@@ -34,7 +35,7 @@ func GetRoomID(chat string) (string, error){
 	var roomID string
 
 	lowercase := strings.ToLower(chat)
-	senders := strings.split(lowercase, ":")
+	senders := strings.Split(lowercase, ":")
 	if len(senders) != 2 {
 		err := fmt.Errorf("Invalid Chat ID '%s', required format A1:A2, current format: ", chat)
 		return "", err
@@ -49,3 +50,17 @@ func GetRoomID(chat string) (string, error){
 	return roomID, nil
 }
 
+func ValidateRequest(req *rpc.SendRequest){
+	senders := strings.Split(req.Message.Chat, ":")
+	if len(senders) != 2 {
+		err := fmt.Errorf("Invalid Chat ID '%s', required format A1:A2, current format: ", req.Message.GetChat())
+		return err
+	}
+	sender1, sender2 := senders[0], senders[1]
+
+	if req.Message.GetSender() != sender1 && req.Message.GetSender() != sender2 {
+		err := fmt.Errorf("sender '%s' not in this chat room", req.Message.GetSender())
+		return err
+	}
+	return nil
+}
