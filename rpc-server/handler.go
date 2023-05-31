8b537668a4cede08153300187a850ee86951b0d5
BuildTools <unconfigured@null.spigotmc.org>
2023-05-31 23:15:51 +0800

    removed dummy functions from handler.go

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -12,25 +12,25 @@ import (
 // IMServiceImpl implements the last service interface defined in the IDL.
 type IMServiceImpl struct{}
 
-func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
-	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
-	return resp, nil
-}
+// func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
+// 	resp := rpc.NewSendResponse()
+// 	resp.Code, resp.Msg = areYouLucky()
+// 	return resp, nil
+// }
 
-func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
-	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
-	return resp, nil
-}
+// func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+// 	resp := rpc.NewPullResponse()
+// 	resp.Code, resp.Msg = areYouLucky()
+// 	return resp, nil
+// }
 
-func areYouLucky() (int32, string) {
-	if rand.Int31n(2) == 1 {
-		return 0, "success"
-	} else {
-		return 500, "unlucky"
-	}
-}
+// func areYouLucky() (int32, string) {
+// 	if rand.Int31n(2) == 1 {
+// 		return 0, "success"
+// 	} else {
+// 		return 500, "unlucky"
+// 	}
+// }
 
 func GetRoomID(chat string) (string, error){
 	var roomID string
