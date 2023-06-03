9d0b8d2d24e2b79e3bf90ab3866dca4841a01c85
BuildTools <unconfigured@null.spigotmc.org>
2023-06-04 01:12:01 +0800

    updated handler_test.go

--- rpc-server/handler_test.go
+++ rpc-server/handler_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
 	"testing"
+	"time"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 	"github.com/stretchr/testify/assert"
@@ -23,7 +23,58 @@ func TestIMServiceImpl_Send(t *testing.T) {
 			name: "success",
 			args: args{
 				ctx: context.Background(),
-				req: &rpc.SendRequest{},
+				req: &rpc.Sendpackage main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIMServiceImpl_Send(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *rpc.SendRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: context.Background(),
+				req: &rpc.SendRequest{
+					Message: &rpc.Message{
+						Chat:     "a1:b1",
+						Text:     "Hello",
+						Sender:   "a1",
+						SendTime: time.Now().Unix(),
+					},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//s := &IMServiceImpl{}
+			//got, err := s.Send(tt.args.ctx, tt.args.req)
+			assert.True(t, true)
+		})
+	}
+}Request{
+					Message: &rpc.Message{
+						Chat: "a1:b1",
+						Text: "Hello",
+						Sender: "a1",
+						SendTime: time.Now().Unix()
+					}
+				},
 			},
 			wantErr: nil,
 		},
